9197cf519e5249ff41cd346c6d03f23e1008dcfa
LaigoRamel <laigoramel@gmail.com>
2016-04-15 17:09:28 +0800

    logic

--- pma-vrmsAdmin/logic/delete_form2Logic.php
+++ pma-vrmsAdmin/logic/delete_form2Logic.php
@@ -8,9 +8,9 @@
 		header('Location: ../registeredAFP.php');
 	}
 	
-	function DeleteMilitaryData($id){
+	function DeleteMilitaryData($m_id){
 		global $database;
-		$query = "DELETE form2_militarypd.*, vehicle_information.* FROM form2_militarypd JOIN vehicle_information ON form2_militarypd.m_vehicle_id=vehicle_information.vehicleId WHERE form2_militarypd.m_vehicle_id='$id' AND vehicle_information.vehicleId='$id'";
+		$query = "DELETE form2_militarypd.*, vehicle_information.* FROM form2_militarypd JOIN vehicle_information ON form2_militarypd.m_vehicle_id=vehicle_information.vehicleId WHERE form2_militarypd.m_vehicle_id='$m_id' AND vehicle_information.vehicleId='$m_id'";
 		$database->execute($query);
 		$database->disconnect();
 	}
