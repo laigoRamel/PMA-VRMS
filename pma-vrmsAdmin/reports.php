e19994f598360827270932f2b76ee43e376d8893
Marc Lim <mjglqw@gmail.com>
2016-04-19 01:06:28 +0800

    Print: temporary siguro

--- pma-vrmsAdmin/reports.php
+++ pma-vrmsAdmin/reports.php
@@ -14,11 +14,13 @@ include('login/session.php');
     <link rel="stylesheet" href="css/style-main.min.css">
     <link rel="stylesheet" href="bootstrap/css/jquery.bdt.css">
     <link rel="stylesheet" href="css/skin.min.css">
+    <link rel="stylesheet" type="text/css" href="css/print.css">
     <link rel="icon" href="img/seal.png">
     
 	<script src="bootstrap/jquery.min.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/app.min.js"></script>
+
 </head>
     
 <body class="hold-transition skin-black sidebar-mini">
@@ -141,8 +143,16 @@ include('login/session.php');
 		<div class="'container">
 			<div class="row">
 				<div class="col-md-12">
+                    
                     <div class="box">
+                        <h3>
+                            <span id="printHeader" class="logo-lg"><b>PMA</b>VRMS: REPORTS</span>
+                        </h3>
                         <div class="box-body">
+                            
+                            <button id="printReports" class="btn btn-default btn-md pull-right" onclick="printReports()">
+                                <span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print
+                            </button>
                             <table class="table table-bordered table-hover" id="bootstrap-table">
                                 <thead>
                                     <th>Plate No.</th>
@@ -170,13 +180,14 @@ DATA;
                                     ?>
                                 </tbody>
                             </table>
-                            <button class="btn btn-default pull-right">Print</button>
                         </div>
                     </div>
                 </div>
-
+            
+            <script src="js/print.js" type="text/javascript"></script>
             <script src="bootstrap/js/jquery.sortelements.js" type="text/javascript"></script>
             <script src="bootstrap/js/jquery.bdt.js" type="text/javascript"></script>
+
             <script>
                 $(document).ready( function () {
                     $('#bootstrap-table').bdt();
