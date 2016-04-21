e9bdc374a2c10b6a0b2082ea88af634d85ed010a
a-l-v-y <alvymangosan@gmail.com>
2016-04-22 05:28:05 +0800

    user delete

--- pma-vrmsAdmin/logic/delete_form2Logic.php
+++ pma-vrmsAdmin/logic/delete_form2Logic.php
@@ -1,5 +1,6 @@
 <?php 
 	require_once('../db/database.php');
+	session_start();
 	
 	$database = new Database();
 	
@@ -14,10 +15,26 @@
 	
 	function DeleteMilitaryData($m_id, $id){
 		global $database;
+
+		date_default_timezone_set("Asia/Hong_Kong");
+
+		$current_date = date("Y-m-d");
+		$current_time = date("h:i:s");
+		$username = $_SESSION['getUser'];
+
+		//$full_name = $a_lastname . ', ' . $a_firstname . ' ' . $a_middlename;
+
+		$query2 = "INSERT INTO admin_logs (id, username, activity, curr_date, curr_time)
+					VALUES ('', '$username', 'Deleted: ', '$current_date', '$current_time')";
+
+		$database->execute($query2);
+
 		$query = "DELETE FROM form2_militarypd WHERE m_militaryId='$m_id'";
 		$database->execute($query);
 		$query1 = "DELETE FROM vehicle_information WHERE vehicleId='$id'";
 		$database->execute($query1);
+
+
 
 		$database->disconnect();
 	}
